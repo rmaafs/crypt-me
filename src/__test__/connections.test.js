c4530a3b10a355622ec5eb1dbab78d79e609d5d5
Rodrigo Maafs Atilano <rodrigoelmaps@gmail.com>
2021-08-15 21:52:17 -0500

    Pruebas unitarias

--- src/__test__/connections.test.js
+++ src/__test__/connections.test.js
@@ -3,26 +3,101 @@ const { default: Server } = require("../server");
 
 const app = new Server();
 const agent = request.agent(app);
-jest.setTimeout(10 * 1000);
+jest.setTimeout(5 * 1000);
+
+beforeAll((done) => {
+  app.on("appStarted", function () {
+    done();
+  });
+});
 
 describe("Verificar que guardan los registros", () => {
-  before(async (done) => {
-    app.on("appStarted", function () {
-      done();
-    });
-  });
+  const text = "UHJ1ZWJh";
+  let id = "";
+  let secret = "";
 
-  after(async (done) => {
-    app.close(done);
-  });
-
-  test("Enviar petición para guardar texto", async () => {
+  test("Encriptar texto", async () => {
     const res = await agent.post("/").send({
       text: "UHJ1ZWJh",
     });
 
     console.log(res.body);
+
+    expect(res.statusCode).toEqual(201);
+    expect(res.body).toHaveProperty("id");
+    expect(res.body).toHaveProperty("secret");
+    expect(res.body).toHaveProperty("end");
+    id = res.body.id;
+    secret = res.body.secret;
+  });
+
+  test("Desencriptar texto", async () => {
+    const res = await agent.patch("/").send({
+      id: id,
+      secret: secret,
+    });
+
+    console.log(res.body);
+
     expect(res.statusCode).toEqual(200);
-    //expect(res.body).toHaveProperty("post");
+    expect(res.body).toHaveProperty("text");
+    expect(res.body.text).toEqual(text);
+  });
+
+  test("Eliminar registro después de consultarlo", async () => {
+    const res = await agent.patch("/").send({
+      id: id,
+      secret: secret,
+    });
+
+    console.log(res.body);
+
+    expect(res.statusCode).toEqual(400);
+    expect(res.body).toHaveProperty("error");
+  });
+});
+
+describe("Evitar fuerza bruta", () => {
+  const text = "UHJ1ZWJh";
+  let id = "";
+  let secret = "";
+
+  test("Encriptar texto", async () => {
+    const res = await agent.post("/").send({
+      text: text,
+    });
+
+    console.log(res.body);
+
+    expect(res.statusCode).toEqual(201);
+    expect(res.body).toHaveProperty("id");
+    expect(res.body).toHaveProperty("secret");
+    expect(res.body).toHaveProperty("end");
+    id = res.body.id;
+    secret = res.body.secret;
+  });
+
+  test("Borrar registro al consultarlo erróneamente", async () => {
+    const res = await agent.patch("/").send({
+      id: id,
+      secret: secret + "a", //Secret erróneo
+    });
+
+    console.log(res.body);
+
+    expect(res.statusCode).toEqual(400);
+    expect(res.body).toHaveProperty("error");
+  });
+
+  test("Verificar que si se borró", async () => {
+    const res = await agent.patch("/").send({
+      id: id,
+      secret: secret,
+    });
+
+    console.log(res.body);
+
+    expect(res.statusCode).toEqual(400);
+    expect(res.body).toHaveProperty("error");
   });
 });
