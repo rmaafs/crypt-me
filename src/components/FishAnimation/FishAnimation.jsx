3bd665fd3344730b142e45e4585a1fcaad9c142e
Rodrigo Maafs Atilano <rodrigoelmaps@gmail.com>
2021-08-23 21:48:22 -0500

    Diseño y fondo con burbujas

---
+++ src/components/FishAnimation/FishAnimation.jsx
@@ -0,0 +1,23 @@
+import React, { Fragment } from "react";
+import "./FishAnimation.css";
+
+const FishAnimation = () => {
+  return (
+    <Fragment>
+      <div id="background-wrap">
+        <div class="bubble x1"></div>
+        <div class="bubble x2"></div>
+        <div class="bubble x3"></div>
+        <div class="bubble x4"></div>
+        <div class="bubble x5"></div>
+        <div class="bubble x6"></div>
+        <div class="bubble x7"></div>
+        <div class="bubble x8"></div>
+        <div class="bubble x9"></div>
+        <div class="bubble x10"></div>
+      </div>
+    </Fragment>
+  );
+};
+
+export default FishAnimation;
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,10 +3,12 @@ import Form from "./Form/Form";
 import { Router } from "@reach/router";
 import "./Main.css";
 import ViewContent from "./ViewContent/ViewContent";
+import FishAnimation from "./FishAnimation/FishAnimation";
 
 const Main = () => {
   return (
     <div className="container">
+      <FishAnimation />
       <Router>
         <Form path="/" />
         <ViewContent path=":id/:secret" />
