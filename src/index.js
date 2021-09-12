aba813709d1f6bef5ad1fb5ea22f0d469fbb8fd6
Rodrigo Maafs Atilano <rodrigoelmaps@gmail.com>
2021-09-12 17:47:44 -0500

    Mensaje en consola

--- src/index.js
+++ src/index.js
@@ -3,3 +3,7 @@ import ReactDOM from "react-dom";
 import App from "./App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
+console.log(
+  "\n\n%c¡Hola desarrollador!\n\nConsulta el código fuente en\nhttps://github.com/rmaafs/crypt-me \n\n\n",
+  "color:aqua"
+);
