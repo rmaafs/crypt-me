2396c14edc4e937c209e58d843eab11fda4a6f01
Rodrigo Maafs Atilano <rodrigoelmaps@gmail.com>
2021-09-12 20:58:34 -0500

    Link para postman

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import App from "./App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 console.log(
-  "\n\n%c¡Hola desarrollador!\n\nConsulta el código fuente en\nhttps://github.com/rmaafs/crypt-me \n\n\n",
-  "color:aqua"
+  "\n\n%c¡Hola desarrollador!\n\nConsulta el código fuente en\nhttps://github.com/rmaafs/crypt-me \n\n%cAPI REST en Postman:\nhttps://documenter.getpostman.com/view/9525350/U16krkhd \n\n\n",
+  "color:aqua",
+  "color:orange"
 );
