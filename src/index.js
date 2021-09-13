290d7d0dd125c3b80a5150b23bc88b895b018467
Rodrigo Maafs Atilano <rodrigoelmaps@gmail.com>
2021-09-12 22:20:23 -0500

    Permitir console logs mios

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(<App />, document.getElementById("root"));
+
+// eslint-disable-next-line no-console
 console.log(
   "\n\n%c¡Hola desarrollador!\n\nConsulta el código fuente en\nhttps://github.com/rmaafs/crypt-me \n\n%cAPI REST en Postman:\nhttps://documenter.getpostman.com/view/9525350/U16krkhd \n\n\n",
   "color:aqua",
